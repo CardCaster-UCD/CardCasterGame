9a71fcd659d86efd7e16e2f60ed8966b6ae86e65
alexisbl14 <ablydon@ucdavis.edu>
2022-05-23 14:29:59 -0700

    new puzzle map

---
+++ Assets/SuperTiled2Unity/Import/cave.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="cave" tilewidth="16" tileheight="16" tilecount="1000" columns="40">
+ <image source="../../../../../Downloads/gfx/gfx/cave.png" width="640" height="400"/>
+ <tile id="285">
+  <animation>
+   <frame tileid="285" duration="150"/>
+   <frame tileid="286" duration="250"/>
+   <frame tileid="287" duration="200"/>
+   <frame tileid="288" duration="250"/>
+  </animation>
+ </tile>
+</tileset>
