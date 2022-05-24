f245dc6b5dde36aa34d5055cd65535892f3950c0
Grant <grantmaingilson@gmail.com>
2022-05-23 19:33:38 -0700

    scene transition works kinda...
    need to have player and other things set up first

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneTransition : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] string SceneName;
+    [SerializeField] Vector2 playerPosition;
+    [SerializeField] VectorValue playerStorage;
+    void OnTriggerEnter2D(Collider2D other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+        if (other.gameObject.tag == "Player" && !other.isTrigger)
+        {
+            playerStorage.initialValue = playerPosition;
+            SceneManager.LoadScene(SceneName);
+        }
+    }    
 }
