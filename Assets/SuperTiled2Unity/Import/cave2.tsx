5f608c25702860e928d709297bad84bee302a40c
alexisbl14 <ablydon@ucdavis.edu>
2022-05-23 22:20:16 -0700

    finalized linking of puzzle room

--- Assets/SuperTiled2Unity/Import/cave2.tsx
+++ Assets/SuperTiled2Unity/Import/cave2.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.8" tiledversion="1.8.5" name="cave" tilewidth="16" tileheight="16" tilecount="336" columns="16">
- <image source="../../../../../../Downloads/cave.png" width="256" height="336"/>
+ <image source="../../Art/cave2.png" width="256" height="336"/>
  <tile id="121">
   <animation>
    <frame tileid="304" duration="200"/>
