af7e1310e4a02ff801b9ceac6463826075aafde0
Grant <grantmaingilson@gmail.com>
2022-05-26 16:02:54 -0700

    added defaultdict data structre

--- Assets/Scripts/Utilities.cs
+++ Assets/Scripts/Utilities.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Utilities
 {
@@ -7,6 +9,28 @@ namespace Utilities
         public static Vector3 ToVector2(this Vector3 vector)
         {
             return new Vector3(vector.x, vector.y, 0);
+        }
+    }
+
+    // Helper class to emulate a python DefaultDictionary
+    // Default values are the type's default value
+    // bool -> false
+    // int -> 0
+    public class DefaultDictionary<TKey, TValue> : Dictionary<TKey, TValue> where TValue : new()
+    {
+        public new TValue this[TKey key]
+        {
+            get
+            {
+                TValue val;
+                if (!TryGetValue(key, out val))
+                {
+                    val = new TValue();
+                    Add(key, val);
+                }
+                return val;
+            }
+            set { base[key] = value; }
         }
     }
 }
