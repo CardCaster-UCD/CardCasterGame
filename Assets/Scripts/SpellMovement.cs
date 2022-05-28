ddd6b7dd54fdf42ecb01b64413fc7aa616263d14
easgouros <easgouros@ucdavis.edu>
2022-05-28 15:32:24 -0700

    Forgot one more script

---
+++ Assets/Scripts/SpellMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellMovement : MonoBehaviour
+{
+    [SerializeField] private Vector2 velocity;
+
+    public void SetVelocity(float x, float y)
+    {
+        this.velocity = new Vector2(x, y);
+    }
+    
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        float currentX = this.transform.position.x;
+        float currentY = this.transform.position.y;
+        float currentZ = this.transform.position.z;
+
+        float xChange = velocity.x * Time.deltaTime;
+        float yChange = velocity.y * Time.deltaTime;
+
+        this.transform.position = new Vector3(currentX + xChange, currentY + yChange, currentZ);
+    }
+}
