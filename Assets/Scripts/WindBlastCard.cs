f16dd41a9d90f744b8211c233d07537e7aa864d5
easgouros <easgouros@ucdavis.edu>
2022-05-28 15:25:23 -0700

    Forgot to add pretty much everything

---
+++ Assets/Scripts/WindBlastCard.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class WindBlastCard : ScriptableObject, ICard
+{
+    private bool isActive = true;
+    private const float speed = 6.0f;
+    private const float lifetime = 5.0f;
+    public void Execute(GameObject player)
+    {
+        var fireball = (GameObject)Resources.Load("Prefabs/Wind", typeof(GameObject));
+        
+        // TODO decide out how to handle diagonal
+        float xVelocity = player.GetComponent<PlayerMovement>().GetFacing().x * speed;
+        float yVelocity = player.GetComponent<PlayerMovement>().GetFacing().y * speed;
+
+        fireball.GetComponent<SpellMovement>().SetVelocity(xVelocity, yVelocity);
+
+        Destroy(Instantiate(fireball, player.transform.position, Quaternion.identity), lifetime);
+
+    }
+    public bool GetIsActive()
+    {
+        return isActive;
+    }
+    public Sprite GetSprite()
+    {
+        // TODO get a sprite for this.
+        return (Sprite)null; 
+    }
+}
