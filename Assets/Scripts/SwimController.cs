9c8e9ff635870e9f4a1c5c855373fc6d59b47847
alexisbl14 <ablydon@ucdavis.edu>
2022-06-01 19:48:06 -0700

    fixed swimming animations with vector interactions

--- Assets/Scripts/SwimController.cs
+++ Assets/Scripts/SwimController.cs
@@ -7,15 +7,27 @@ public class SwimController : MonoBehaviour
     [SerializeField] private Animator animator;
     private bool playerInWater = false;
 
+    private float horizontal;
+    private float vertical;
+
+
     // Start is called before the first frame update
     // Update is called once per frame
     void Update()
     {
         if(playerInWater)
-        {   
+        {
             if(animator.GetBool("Swim") == false)
             {
                 animator.SetBool("Swim", true);
+            }
+            if((Input.GetAxisRaw("Horizontal") == 0) && (Input.GetAxisRaw("Vertical") == 0))
+            {
+                Debug.Log("enter the if statement");
+                //Vector2 velocity = this.gameObject.GetComponent<Rigidbody2D>().velocity;
+                //Debug.Log(velocity);
+                animator.SetFloat("Horizontal", horizontal);
+                animator.SetFloat("Vertical", vertical);
             }
         }
         else
@@ -32,7 +44,22 @@ public class SwimController : MonoBehaviour
         if(collider.CompareTag("Water"))
         {
             playerInWater = true;
-            
+        }
+        if(collider.CompareTag("WaterPushUp"))
+        {
+            vertical = 1.0f;
+        }
+        else if(collider.CompareTag("WaterPushDown"))
+        {
+            vertical = -1.0f;
+        }
+        else if(collider.CompareTag("WaterPushLeft"))
+        {
+            horizontal = -1.0f;
+        }
+        else if(collider.CompareTag("WaterPushRight"))
+        {
+            horizontal = 1.0f;
         }
     }
 
@@ -42,6 +69,22 @@ public class SwimController : MonoBehaviour
         if(collider.CompareTag("Water"))
         {
             playerInWater = false;
+        }
+        if(collider.CompareTag("WaterPushUp"))
+        {
+            vertical = 0.0f;
+        }
+        else if(collider.CompareTag("WaterPushDown"))
+        {
+            vertical = 0.0f;
+        }
+        else if(collider.CompareTag("WaterPushLeft"))
+        {
+            horizontal = 0.0f;
+        }
+        else if(collider.CompareTag("WaterPushRight"))
+        {
+            horizontal = 0.0f;
         }
     }
 }
