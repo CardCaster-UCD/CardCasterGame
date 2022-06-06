fb60f556623606dc2222773968e6e2081e13fb50
alexisbl14 <ablydon@ucdavis.edu>
2022-06-06 10:08:13 -0700

    added comments to scripts

--- Assets/Scripts/SwimController.cs
+++ Assets/Scripts/SwimController.cs
@@ -4,23 +4,28 @@ using UnityEngine;
 
 public class SwimController : MonoBehaviour
 {
+    // Reference to the animator on the player.
     [SerializeField] private Animator animator;
+    // Boolean representing whether or not the player is in the water.
     private bool playerInWater = false;
-
+    // Horizontal and vertical values used when there is no player input.
     private float horizontal;
     private float vertical;
 
 
-    // Start is called before the first frame update
-    // Update is called once per frame
+    // Check to see if playerInWater is true.
     void Update()
     {
         if(playerInWater)
         {
+            // If player in water and the Swim bool in the animator not set to true, set animator to true.
             if(animator.GetBool("Swim") == false)
             {
                 animator.SetBool("Swim", true);
             }
+            // Check if there is any player input.
+            // If there is no player input, set horizontal and vertical "input" according to which water they are in.
+            // This makes them continuously get taken by the river even if they are not using any keys.
             if((Input.GetAxisRaw("Horizontal") == 0) && (Input.GetAxisRaw("Vertical") == 0))
             {
                 animator.SetFloat("Horizontal", horizontal);
@@ -29,6 +34,7 @@ public class SwimController : MonoBehaviour
         }
         else
         {
+            // If player is not in water, set Swim bool to false.
             if(animator.GetBool("Swim") == true)
             {
                 animator.SetBool("Swim", false);
@@ -36,12 +42,16 @@ public class SwimController : MonoBehaviour
         }
     }
 
+    // Check if water entered player's trigger collider.
     private void OnTriggerEnter2D(Collider2D collider)
     {
+        // Hitting any water sets playerInWater to true.
         if(collider.CompareTag("Water"))
         {
             playerInWater = true;
         }
+        // Hitting specific streams adjusts the vertical and horizontal automatic movement to direction
+        // of stream they are in and sets the vertical and horizontal values accordingly.
         if(collider.CompareTag("WaterPushUp"))
         {
             vertical = 1.0f;
@@ -60,13 +70,16 @@ public class SwimController : MonoBehaviour
         }
     }
 
-    // When collider exits the BoxCollider
+    // When collider exits the BoxCollider.
+    // Checking here if water exits player's trigger BoxCollider.
     private void OnTriggerExit2D(Collider2D collider)
     {
+        // Leaving any water sets playerInWater to false.
         if(collider.CompareTag("Water"))
         {
             playerInWater = false;
         }
+        // Leaving any specific stream of water sets the horizontal and vertical default movement to 0.
         if(collider.CompareTag("WaterPushUp"))
         {
             vertical = 0.0f;
